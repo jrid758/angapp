29fdf3eea168a7d4a72bd470f9b45d7dea2129c4
Jason R <marathon758@gmail.com>
2017-12-21 06:46:34 -0600

    starting on ng sort

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OrderListModule } from 'primeng/primeng';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  cars:ICars[];
+  test2: string = 'hello';
+
+
+  ngOnInit() {
+        this.cars = [{brand: "Happy", year: 1989, color:"Red"}, {brand: "Happy2", year: 19892, color:"Red2"}];
+    }
+
+
+}
+
+export interface ICars {
+  brand: string;
+  year: number;
+  color: string;
 }
