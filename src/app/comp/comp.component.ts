549e0f05fc74cd8d19be8ac4cbe7a666c028b559
Jason R <marathon758@gmail.com>
2018-01-11 06:23:04 -0600

    Figuring out how to appendChild pixi

--- src/app/comp/comp.component.ts
+++ src/app/comp/comp.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnInit, ViewChild, ElementRef, AfterViewInit,Renderer2 } from "@angular/core";
 import * as PIXI from 'pixi.js';
 
 @Component({
@@ -8,9 +8,16 @@ import * as PIXI from 'pixi.js';
 
 })
 
-export class CompComponent implements OnInit{
+export class CompComponent implements OnInit, AfterViewInit {
+
+
+    @ViewChild('compView') compView: ElementRef;
     time: string = "hello";
     htmlToAdd;
+
+    constructor(private renderer: Renderer2) {
+
+    }
 
     ngOnInit() {
         let type = "WebGL"
@@ -21,9 +28,20 @@ export class CompComponent implements OnInit{
         PIXI.utils.sayHello(type)
 
         let app = new PIXI.Application({width: 256, height: 256});
+        console.log("whats in app:" + app.view);
+        //this.renderer.appendChild(app.view);
+        
         //this.htmlToAdd = app.view;
         //this.htmlToAdd = '<div class="two">two</div>';
         //document.body.appendChild(app.view);
+        //this.compView.nativeElement.appendChild(app.view);
+        //this.compView.nativeElement.appendChild(app.view);
     }
+
+     ngAfterViewInit(): void {
+            //throw new Error('Method not implemented.');
+            //this.compView.nat = "hello2";
+            
+     }
 
 }
